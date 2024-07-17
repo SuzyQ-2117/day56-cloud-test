fabd57903ca1fc73d07e22aaad87c42c76854445
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-07-17 10:15:04 +0100

    updated react app with minor change

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import myImage from './images/my-image.jpg';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>My 1st React App</h1>
-        <p>Everyday is a school day</p>
+        <Welcome />
       </header>
       <div className="image-wrapper">
         <img src={myImage} alt="Classroom" />
